813282ec16213185bfecd51a740ec01dddea151f
momen <abdulmomen.official@gmail.com>
2026-02-22 15:34:24 +0600

    DOTNET

---
+++ Practice01/Program.cs
@@ -0,0 +1,42 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+        
+//         Console.WriteLine("Triangle Base Problem");
+
+//         for(int i=0; i<4; i++)
+//         {
+//             for(int j=0; j <= i; j++)
+//             {
+//                 Console.Write("*");
+//             }
+//             Console.WriteLine();
+//         }
+
+
+       
+//     }
+// }
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 4;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
