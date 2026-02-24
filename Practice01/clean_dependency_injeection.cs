a6623908db02dc772b4724541b2fbbefa3ba712a
momen <abdulmomen.official@gmail.com>
2026-02-24 16:08:31 +0600

    cleaned Dependenjecy Injection updated way

---
+++ Practice01/clean_dependency_injeection.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace Practice01
+{
+    public interface ITool
+    {
+        void Use();
+    }
+
+    public class Hammer : ITool
+    {
+        public void Use() => Console.WriteLine("Hammering Nails");
+    }
+
+    public class Saw : ITool
+    {
+        public void Use() => Console.WriteLine("Sawing Wood");
+    }
+
+    public class Builder
+    {
+        private List<ITool> _tools;
+
+        public Builder(IEnumerable<ITool> tools)
+        {
+            _tools = new List<ITool>(tools);
+        }
+
+        public void BuildHouse()
+        {
+            foreach (var tool in _tools)
+                tool.Use();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var tools = new List<ITool> { new Hammer(), new Saw() };
+            Builder builder = new Builder(tools);
+            builder.BuildHouse();
+        }
+    }
+}
